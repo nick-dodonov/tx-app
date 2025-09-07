7109d8013c039d549d1c75b1331361fad2772e42
Nick Dodonov <42121323+nick-dodonov@users.noreply.github.com>
2025-09-07 18:33:16 +0200

    vscode settings for wsp pkg via sources by default

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <Shared/Version.h>
 
 int main(int argc, char** argv) {
-    std::cout << "hello world!" << std::endl;
+    std::cout << "Hello world!" << std::endl;
     std::cout << GetVersionDetailed() << std::endl;
     std::cout << "add = " << add(1, 2) << std::endl;    
     return 0;
