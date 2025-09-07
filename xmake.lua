7a0245ff25660e65ecac9269bf930a8d1218f7c3
Nick Dodonov <42121323+nick-dodonov@users.noreply.github.com>
2025-09-07 15:27:27 +0200

    renames in deps and current repo allowing to split

--- xmake.lua
+++ xmake.lua
@@ -1,17 +1,17 @@
 add_rules("mode.debug", "mode.release")
 includes("xmake/modules/tx_dep.lua")
 
-tx_add_package_dep("tx-lib-shared")
+tx_add_package_dep("tx-pkg-misc")
 
-target("tx-app")
+target("tx-app-demo")
     set_kind("binary")
     add_files("src/*.cpp")
-    tx_add_target_dep("tx-lib-shared")
+    tx_add_target_dep("tx-pkg-misc")
 
-package("tx-lib-shared")
-    add_urls("https://github.com/nick-dodonov/tx-lib-shared.git")
+package("tx-pkg-misc")
+    add_urls("https://github.com/nick-dodonov/tx-pkg-misc.git")
 
-    --set_sourcedir(path.join(os.scriptdir(), "../tx-lib-shared"))
+    --set_sourcedir(path.join(os.scriptdir(), "../tx-pkg-misc"))
     --set_policy("package.install_always", true)
 
     on_install(function (package)
