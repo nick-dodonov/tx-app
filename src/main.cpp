b0d1b0541da4ded6230eeaea3249d3ada2ddd0e1
Nick Dodonov <42121323+nick-dodonov@users.noreply.github.com>
2025-09-07 11:00:49 +0200

    prepare to test shared lib switch package<->source

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <foo.h>
+#include <Shared/Version.h>
 
 int main(int argc, char** argv) {
     std::cout << "hello world!" << std::endl;
-    add(1, 2);
+    std::cout << GetVersionDetailed() << std::endl;
+    std::cout << "add = " << add(1, 2) << std::endl;    
     return 0;
 }
-
