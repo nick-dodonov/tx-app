d13161655963e5241d235c4b06b6c0ebeb3244f0
Nick Dodonov <42121323+nick-dodonov@users.noreply.github.com>
2025-09-08 22:32:01 +0200

    just simple sdl wrapper was used to test xmake is useful

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
 #include <Shared/Version.h>
+#include <centurion.hpp>
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
     std::cout << "Hello world!" << std::endl;
     std::cout << GetVersionDetailed() << std::endl;
+
+    // try sdl wrapper
+    const cen::sdl sdl;
+    const cen::img img;
+    const cen::mix mix;
+    const cen::ttf ttf;
+
+    cen::window window{"Hello world!"};
+    cen::renderer renderer = window.make_renderer();
+
+    window.show();
+
+    bool running = true;
+    while (running) {
+        cen::event_handler event;
+        while (event.poll()) {
+            if (event.is(cen::event_type::quit)) {
+                running = false;
+            }
+        }
+        renderer.clear_with(cen::colors::coral);
+        renderer.present();
+    }
+
+    window.hide();
     return 0;
 }
--- xmake.lua
+++ xmake.lua
@@ -1,4 +1,5 @@
 add_rules("mode.debug", "mode.release")
+set_languages("c++17")
 
 add_repositories("tx-kit-repo " .. (os.getenv("TX_REPO") or "git@github.com:nick-dodonov/tx-kit-repo.git main"))
 add_repositories("local-repo ./xmake")
@@ -7,9 +8,11 @@ add_requires("tx-kit")
 includes(".xmake/tx-kit/includes")
 
 add_requires("tx-pkg-misc")
+add_requires("centurion")
 
 target("tx-app-demo", function () 
     set_kind("binary")
     add_files("src/*.cpp")
     add_packages("tx-pkg-misc")
+    add_packages("centurion")
 end)
