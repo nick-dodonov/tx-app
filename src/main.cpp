98c252b78637afa7bd51f0e095db6090b13eb5fb
Nick Dodonov <42121323+nick-dodonov@users.noreply.github.com>
2025-09-08 14:09:28 +0200

    rm wsp xmake settings (they are used in pkg too); use env instead

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
-#include <foo.h>
 #include <Shared/Version.h>
 
 int main(int argc, char** argv) {
     std::cout << "Hello world!" << std::endl;
     std::cout << GetVersionDetailed() << std::endl;
-    std::cout << "add = " << add(1, 2) << std::endl;    
     return 0;
 }
--- xmake/includes/tx_kit/tx_kit.lua
+++ xmake/includes/tx_kit/tx_kit.lua
@@ -1,19 +1,20 @@
 -- Extended dependency management, allows using local pkg repos for develop
---  xmake f -cy && xmake run -yv                # clean/configure/run using remote pkg repos
---  xmake f -cy --use_local=1 && xmake run -yv  # clean/configure/run for local pkg repos
-local USE_LOCAL = "use_local"
-option(USE_LOCAL, {default = false})
+local KEY_REPO = "TX_LOCAL_REPO"
+local KEY_PKG = "TX_LOCAL_PKG"
+
+option(KEY_REPO, {default = os.getenv(KEY_REPO)})
+option(KEY_PKG, {default = os.getenv(KEY_PKG)})
 
 function tx_kit_add_repo(name)
-    if has_config(USE_LOCAL) then
-        add_repositories(name, "../" .. name)
+    if has_config(KEY_REPO) then
+        add_repositories(name .. " ../" .. name)
     else
-        add_repositories(name, "git@github.com:nick-dodonov/" .. name .. ".git")
+        add_repositories(name .. " git@github.com:nick-dodonov/" .. name .. ".git")
     end
 end
 
 function tx_kit_add_pkg_need(name)
-    if has_config(USE_LOCAL) then
+    if has_config(KEY_PKG) then
         includes(os.scriptdir() .. "/../" .. name)
     else
         add_requires(name, {system = false})
@@ -21,7 +22,7 @@ function tx_kit_add_pkg_need(name)
 end
 
 function tx_kit_add_pkg_deps(name)
-    if has_config(USE_LOCAL) then
+    if has_config(KEY_PKG) then
         add_deps(name)
     else
         add_packages(name)
