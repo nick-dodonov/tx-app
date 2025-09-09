a0facfaf5c21230d7e4b40ce1ca719d2f66fb742
Nick Dodonov <42121323+nick-dodonov@users.noreply.github.com>
2025-09-10 00:27:23 +0200

    try and setup clangd in vscode for use compile commands from xmake

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,10 @@
 
 int main(int argc, char** argv)
 {
-	std::cout << "Hello world!" << std::endl;
-	std::cout << GetVersionDetailed() << std::endl;
+	std::cout << "Hello world!\n";
+	for (int i = 0; i < argc; ++i) {
+		std::cout << "Arg " << i << ": " << argv[i] << '\n';
+	}
+	std::cout << GetVersionDetailed() << '\n';
 	return 0;
 }
